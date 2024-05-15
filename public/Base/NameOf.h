08f18beeaa7e344315632c8dcebcbea478529f43
Hinageshi <69386319+Hinageshi01@users.noreply.github.com>
2024-05-15 18:04:10 +0800

    Upgrade nameof and implement enumof_name (#7)

--- public/Base/NameOf.h
+++ public/Base/NameOf.h
@@ -2,13 +2,33 @@
 
 #include <nameof.hpp>
 
+#include <optional>
+
 namespace nameof
 {
 
 template <typename E>
-[[nodiscard]] constexpr auto enum_count() noexcept
+[[nodiscard]] inline constexpr auto enum_count() noexcept
 {
-	return nameof::detail::count_v<std::decay_t<E>>;
+	return detail::count_v<std::decay_t<E>>;
+}
+
+template<typename E>
+[[nodiscard]] inline std::optional<E> enumof_name(const char* name) noexcept
+{
+	using D = std::decay_t<E>;
+	static_assert(detail::nameof_enum_supported<D>::value, "nameof unsupported compiler (https://github.com/Neargye/nameof#compiler-compatibility).");
+	static_assert(detail::count_v<D> > 0, "nameof requires enum implementation and valid max and min.");
+
+	for (std::size_t i = 0; i < detail::count_v<D>; ++i)
+	{
+		if (detail::names_v<D>[i].compare(name))
+		{
+			return detail::enum_value<D>(i);
+		}
+	}
+
+	return std::nullopt;
 }
 
 }
